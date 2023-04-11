86d0991fd25810ad9e7661ad295a0ffc74248014
Glendell Bringino <dell@Glendells-MacBook-Pro.local>
2023-04-11 23:47:46 +0800

    update lvim

--- config.lua
+++ config.lua
@@ -8,7 +8,7 @@ an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
-
+-- TODO:
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
@@ -36,21 +36,22 @@ lvim.builtin.treesitter.rainbow.enable = true
 -- vim.g.gitblame_ignored_filetypes = { "NvimTree" }
 -- vim.g.gitblame_display_virtual_text = 0
 vim.g.markdown_fenced_languages = {
-    "ts=typescript"
+  "ts=typescript"
 }
+
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
--- lvim.keys.normal_mode["<C-x>"] = ":MinimapToggle<cr>"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-Up>"] = ":m-2<CR>"
 lvim.keys.normal_mode["<S-Down>"] = ":m+<CR>"
-lvim.keys.normal_mode["<C-g>"] = ":GitBlameToggle<cr>"
+-- lvim.keys.normal_mode["<C-g>"] = ":GitBlameToggle<cr>"
 lvim.keys.normal_mode["<Tab>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-Tab>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<Leader>n"] = ":NvimTreeFocus<CR>"
-
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -81,43 +82,66 @@ lvim.keys.normal_mode["<Leader>n"] = ":NvimTreeFocus<CR>"
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-    name = "+Trouble",
-    r = { "<cmd>Trouble lsp_references<cr>", "References" },
-    f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-    d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-    q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-    l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-    w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
 
 lvim.builtin.which_key.mappings["v"] = {
-    name = "+Diffview",
-    o = { "<cmd>DiffviewOpen<cr>", "Open" },
-    c = { "<cmd>DiffviewClose<cr>", "Close" },
-    t = { "<cmd>DiffviewToggleFiles<cr>", "Toggle Files" },
-    f = { "<cmd>DiffviewFocusFiles<cr>", "Focus Files" },
-    r = { "<cmd>DiffviewRefresh<cr>", "Refresh" }
+  name = "+Diffview",
+  o = { "<cmd>DiffviewOpen<cr>", "Open" },
+  c = { "<cmd>DiffviewClose<cr>", "Close" },
+  t = { "<cmd>DiffviewToggleFiles<cr>", "Toggle Files" },
+  f = { "<cmd>DiffviewFocusFiles<cr>", "Focus Files" },
+  r = { "<cmd>DiffviewRefresh<cr>", "Refresh" }
+}
+
+vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Rust",
+  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+  t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+  R = {
+    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+    "Reload Workspace",
+  },
+  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+  y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+  P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+  i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+  f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+  D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
 }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify = false
+lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "yaml",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -164,47 +188,45 @@ lvim.builtin.treesitter.highlight.enable = true
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 
-
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-    { command = "black", filetypes = { "python" } },
-    { command = "isort", filetypes = { "python" } },
-    {
-        -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-        command = "prettierd",
-        ---@usage arguments to pass to the formatter
-        -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-        -- extra_args = { "--print-with", "100" },
-        ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tsx", "jsx", "css", "scss" },
-    },
+  { command = "black", filetypes = { "python" } },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettierd",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    -- extra_args = { "--print-with", "100" },
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tsx", "jsx", "css", "scss" },
+  },
 }
 
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-    { command = "flake8", filetypes = { "python" } },
-    -- {
-    --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    --   command = "shellcheck",
-    --   ---@usage arguments to pass to the formatter
-    --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    --   extra_args = { "--severity", "warning" },
-    -- },
-    {
-        command = "eslint_d",
-        ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tsx", "jsx" },
-    },
+  { command = "flake8", filetypes = { "python" } },
+  -- {
+  --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --   command = "shellcheck",
+  --   ---@usage arguments to pass to the formatter
+  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --   extra_args = { "--severity", "warning" },
+  -- },
+  {
+    command = "eslint_d",
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tsx", "jsx" },
+  },
 }
 
 local code_actions = require "lvim.lsp.null-ls.code_actions"
 code_actions.setup {
-    {
-        exe = "eslint_d",
-        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tsx", "jsx" },
-    }
+  {
+    exe = "eslint_d",
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tsx", "jsx" },
+  }
 }
 
 local macchiato = require("catppuccin.palettes").get_palette "macchiato"
@@ -212,67 +234,102 @@ local ucolors = require "catppuccin.utils.colors"
 
 -- Additional Plugins
 lvim.plugins = {
-    {
-        "folke/trouble.nvim",
-        cmd = "TroubleToggle",
-    },
-    { "catppuccin/nvim", as = "catppuccin",
-        require("catppuccin").setup({
-            transparent_background = true,
-            color_overrides = {
-                all = {
-                    lavender = macchiato.flamingo,
-                }
-            },
-            highlight_overrides = {
-                all = {
-                    VertSplit = { fg = macchiato.base },
-                    CursorLine = { bg = macchiato.mantle },
-                    DiagnosticVirtualTextError = { bg = ucolors.darken(macchiato.red, 0.3) },
-                    DiagnosticVirtualTextWarn = { bg = ucolors.darken(macchiato.yellow, 0.3) },
-                    DiagnosticVirtualTextInfo = { bg = ucolors.darken(macchiato.blue, 0.3) },
-                    DiagnosticVirtualTextHint = { bg = ucolors.darken(macchiato.teal, 0.3) }
-                }
-            },
-
-            integrations = {
-                indent_blankline = {
-                    enabled = true,
-                    colored_indent_levels = true
-                }
-            }
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    require("catppuccin").setup({
+      transparent_background = true,
+      color_overrides = {
+        all = {
+          lavender = macchiato.flamingo,
+        }
+      },
+      highlight_overrides = {
+        all = {
+          VertSplit = { fg = macchiato.base },
+          CursorLine = { bg = macchiato.mantle },
+          DiagnosticVirtualTextError = { bg = ucolors.darken(macchiato.red, 0.3) },
+          DiagnosticVirtualTextWarn = { bg = ucolors.darken(macchiato.yellow, 0.3) },
+          DiagnosticVirtualTextInfo = { bg = ucolors.darken(macchiato.blue, 0.3) },
+          DiagnosticVirtualTextHint = { bg = ucolors.darken(macchiato.teal, 0.3) }
+        }
+      },
+      integrations = {
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = true
+        }
+      }
+    })
+  },
+  { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' },
+  -- { "f-person/git-blame.nvim" },
+  { "windwp/nvim-ts-autotag", require("nvim-ts-autotag").setup() },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript", "javascriptreact", "typescript",
+          "typescriptreact", "tsx", "jsx" },
+        {
+          RGB = true,      -- #RGB hex codes
+          RRGGBB = true,   -- #RRGGBB hex codes
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn = true,   -- CSS rgb() and rgba() functions
+          hsl_fn = true,   -- CSS hsl() and hsla() functions
+          css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
         })
-    },
-    { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' },
-    -- { "f-person/git-blame.nvim" },
-    { "windwp/nvim-ts-autotag", require("nvim-ts-autotag").setup() },
-    { "norcalli/nvim-colorizer.lua",
-        config = function()
-          require("colorizer").setup({ "css", "scss", "html", "javascript", "javascriptreact", "typescript",
-              "typescriptreact", "tsx", "jsx" },
-              {
-                  RGB = true, -- #RGB hex codes
-                  RRGGBB = true, -- #RRGGBB hex codes
-                  RRGGBBAA = true, -- #RRGGBBAA hex codes
-                  rgb_fn = true, -- CSS rgb() and rgba() functions
-                  hsl_fn = true, -- CSS hsl() and hsla() functions
-                  css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                  css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-              })
-        end,
-    },
-    { "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-
-        event = "BufRead",
-        config = function()
-          require("todo-comments").setup()
-        end,
-    },
-    { 'karb94/neoscroll.nvim',         require('neoscroll').setup() },
-    { "romgrk/nvim-treesitter-context" },
-    -- { "mg978/vim-visual-multi" },
-    { "mrjones2014/nvim-ts-rainbow" }
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+  { 'karb94/neoscroll.nvim',         require('neoscroll').setup() },
+  { "romgrk/nvim-treesitter-context" },
+  -- { "mg978/vim-visual-multi" },
+  { 'otavioschwanck/cool-substitute.nvim', require 'cool-substitute'.setup({
+    setup_keybindings = true, }) },
+  { "mrjones2014/nvim-ts-rainbow" },
+  { "princejoogie/tailwind-highlight.nvim" },
+  { 'simrat39/rust-tools.nvim' },
+  {
+    "saecki/crates.nvim",
+    tag = "v0.3.0",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup {
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+        popup = {
+          border = "rounded",
+        },
+      }
+    end,
+  },
+  -- {
+  --   "j-hui/fidget.nvim",
+  --   config = function()
+  --     require("fidget").setup({
+  --       text = {
+  --         spinner = "dots",
+  --       },
+  --       window = {
+  --         blend = 0
+  --       }
+  --     })
+  --   end,
+  -- },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -288,4 +345,110 @@ lvim.plugins = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
---
+
+local tw_highlight = require('tailwind-highlight')
+
+require('lspconfig').tailwindcss.setup({
+  on_attach = function(client, bufnr)
+    -- rest of you config
+    tw_highlight.setup(client, bufnr, {
+      single_column = false,
+      mode = 'background',
+      debounce = 200,
+    })
+  end,
+  settings = {
+    tailwindCSS = {
+      experimental = {
+        classRegex = {
+          "tw`([^`]*)",          -- tw`...`
+          "tw='([^']*)",         -- <div tw="..." />
+          "tw={`([^`}]*)",       -- <div tw={"..."} />
+          "tw\\.\\w+`([^`]*)",   -- tw.xxx`...`
+          "tw\\(.*?\\)`([^`]*)", -- tw(Component)`...`
+          "styled\\(.*?, '([^']*)'\\)"
+        }
+      }
+    }
+  }
+})
+
+-- require('lspconfig').rust_analyzer.setup {
+--   settings = {
+--     ['rust-analyzer'] = {
+--       checkOnSave = {
+--         allFeatures = true,
+--         overrideCommand = {
+--           'cargo', 'clippy', '--workspace', '--message-format=json',
+--           '--all-targets', '--all-features'
+--         }
+--       }
+--     }
+--   }
+-- }
+
+-- require('lspconfig').rust_analyzer.setup({
+--   settings = {
+--     ['rust-analyzer'] = {
+--       checkOnSave = {
+--         command = "clippy"
+--       }
+--     }
+--   }
+-- })
+
+local rt = require("rust-tools")
+
+rt.setup({
+  tools = {
+    executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+    reload_workspace_from_cargo_toml = true,
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      only_current_line = false,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "<-",
+      other_hints_prefix = "=>",
+      max_len_align = false,
+      max_len_align_padding = 1,
+      right_align = false,
+      right_align_padding = 7,
+      highlight = "Comment",
+    },
+    hover_actions = {
+      border = "rounded",
+    },
+    on_initialized = function()
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+        pattern = { "*.rs" },
+        callback = function()
+          local _, _ = pcall(vim.lsp.codelens.refresh)
+        end,
+      })
+    end,
+  },
+  -- dap = {
+  --   adapter = codelldb_adapter,
+  -- },
+  server = {
+    on_attach = function(client, bufnr)
+      require("lvim.lsp").common_on_attach(client, bufnr)
+      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+    end,
+    capabilities = require("lvim.lsp").common_capabilities(),
+    settings = {
+      ["rust-analyzer"] = {
+        lens = {
+          enable = true,
+        },
+        checkOnSave = {
+          enable = true,
+          command = "clippy",
+        },
+      },
+    },
+  },
+})
